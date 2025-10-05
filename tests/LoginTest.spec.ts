efc4e6fb4c9f42e8c18bf2dadeda292712665503
fkoki0309 <fkoki0309@gmail.com>
2025-10-05 22:55:34 +0900

    email test

--- tests/LoginTest.spec.ts
+++ tests/LoginTest.spec.ts
@@ -4,14 +4,16 @@ test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:5173/');
 });
 
-test("email error", async ({ page }) => {
+test("login error", async ({ page }) => {
+  //email error
   await page.getByPlaceholder('Email').fill('test');
   await page.getByPlaceholder('login').click();
   await expect(page.getByPlaceholder('errorMessage')).toContainText('正しいメールアドレスを入力してください');
 
 });
 
-test("email success", async ({ page }) => {
+test("login success", async ({ page }) => {
+  //email success
   await page.getByPlaceholder('Email').fill('test@example.com');
   await page.getByPlaceholder('login').click();
 
