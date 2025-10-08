92471a79aa26dc4c9e3f23efef1376bb724e010c
fkoki0309 <fkoki0309@gmail.com>
2025-10-08 20:15:55 +0900

    レンダリングのテストのみに変更

--- src/__test__/LoginRender.test.js
+++ src/__test__/LoginRender.test.js
@@ -4,39 +4,14 @@ import Login from '../pages/Login';
 
 // 子コンポーネントをモック化
 jest.mock('../pages/components/EmailInput', () => {
-  return ({ value, onChange, emailError }) => (
-    <div>
-      <input
-        data-testid="email-input"
-        value={value}
-        onChange={onChange}
-      />
-      {emailError && <p data-testid="email-error">{emailError}</p>}
-    </div>
-  );
+  return () => <div data-testid="email-input" />;
 });
-
 jest.mock('../pages/components/PasswordInput', () => {
-  return ({ value, onChange, passwordError }) => (
-    <div>
-      <input
-        data-testid="password-input"
-        value={value}
-        onChange={onChange}
-      />
-      {passwordError && <p data-testid="password-error">{passwordError}</p>}
-    </div>
-  );
+  return () => <div data-testid="password-input" />;
 });
-
 jest.mock('../pages/components/SubmitButton', () => {
-  return ({ buttonText, onClick }) => (
-    <button data-testid="submit-button" onClick={onClick}>
-      {buttonText}
-    </button>
-  );
+  return () => <div data-testid="submit-button" />;
 });
-
 
 describe('Login Component', () => {
   //コンポーネントが正しくレンダリングされるか
